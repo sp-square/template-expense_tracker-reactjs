1d9bf8ed2edd8f080349418ef738ad1afdcfa566
Sandrine Poissonnet <spoisson@gmail.com>
2021-10-24 08:06:55 -0700

    basics covered

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# app-expenseTracker-React-UdemyMS
+# template-expense_tracker-reactjs
+
+This template was built following Maximilian Schwarzmuller's Udemy course "React - The Complete Guide (including Hooks, React Router, Redux).
+
+## Technologies used
+
+- javascript
+- reactjs
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+
+const Expenses = ({ expenses }) => {
+	return (
+		<Card className="expenses">
+			{expenses.map((expense) => (
+				<ExpenseItem
+					key={expense.id}
+					title={expense.title}
+					amount={expense.amount}
+					date={expense.date}
+				/>
+			))}
+		</Card>
+	);
+};
+
+export default Expenses;
